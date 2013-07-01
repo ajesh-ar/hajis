0cc7c4ce12d931b7a1f7eccc3b0ae22cec2927ad
prince <prince@mobomo.com>
2013-07-01 14:27:22 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hajis::Application.config.secret_token = 'af46e752be77ad2b1def5a7b30f80973a7b5c27f13f661cd4ba555158d1a66f6b1834de6bc3a70928a2303cc71c29f4cb927465b0ff430a41e6c1c37603db1b5'
